029f577baba1338248cb38508ba1bf848cde3dd3
heleperezcr <heleperezcr@ittepic.edu.mx>
2022-10-23 17:26:58 -0500

    contiene todas las cartas y audios y fondo

--- app/src/main/java/com/example/ladm_u2_practica1_barajaloteria/MainActivity.kt
+++ app/src/main/java/com/example/ladm_u2_practica1_barajaloteria/MainActivity.kt
@@ -13,8 +13,18 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    val cartas = arrayOf(R.drawable.a1,R.drawable.a2,R.drawable.a3,R.drawable.a4,R.drawable.a5,R.drawable.a6)
-    val sonidos = arrayOf(R.raw.b1,R.raw.b2,R.raw.b3,R.raw.b4,R.raw.b5,R.raw.b6)
+    val cartas = arrayOf(R.drawable.a1,R.drawable.a2,R.drawable.a3,R.drawable.a4,R.drawable.a5,R.drawable.a6,R.drawable.a7,R.drawable.a8,R.drawable.a9,R.drawable.a10,
+                         R.drawable.a11,R.drawable.a12,R.drawable.a13,R.drawable.a14,R.drawable.a15,R.drawable.a16,R.drawable.a17,R.drawable.a18,R.drawable.a19,R.drawable.a20,
+                         R.drawable.a21,R.drawable.a22,R.drawable.a23,R.drawable.a24,R.drawable.a25,R.drawable.a26,R.drawable.a27,R.drawable.a28,R.drawable.a29,R.drawable.a30,
+                         R.drawable.a31,R.drawable.a32,R.drawable.a33,R.drawable.a34,R.drawable.a35,R.drawable.a36,R.drawable.a37,R.drawable.a38,R.drawable.a39,R.drawable.a40,
+                         R.drawable.a41,R.drawable.a42,R.drawable.a43,R.drawable.a44,R.drawable.a45,R.drawable.a46,R.drawable.a47,R.drawable.a48,R.drawable.a49,R.drawable.a50,
+                         R.drawable.a51,R.drawable.a52,R.drawable.a53,R.drawable.a54)
+    val sonidos = arrayOf(R.raw.b1,R.raw.b2,R.raw.b3,R.raw.b4,R.raw.b5,R.raw.b6,R.raw.b7,R.raw.b8,R.raw.b9,R.raw.b10,
+                          R.raw.b11,R.raw.b12,R.raw.b13,R.raw.b14,R.raw.b15,R.raw.b16,R.raw.b17,R.raw.b18,R.raw.b19,R.raw.b20,
+                          R.raw.b21,R.raw.b22,R.raw.b23,R.raw.b24,R.raw.b25,R.raw.b26,R.raw.b27,R.raw.b28,R.raw.b29,R.raw.b30,
+                          R.raw.b31,R.raw.b32,R.raw.b33,R.raw.b34,R.raw.b35,R.raw.b36,R.raw.b37,R.raw.b38,R.raw.b39,R.raw.b40,
+                          R.raw.b41,R.raw.b42,R.raw.b43,R.raw.b44,R.raw.b45,R.raw.b46,R.raw.b47,R.raw.b48,R.raw.b49,R.raw.b50,
+                          R.raw.b51,R.raw.b52,R.raw.b53,R.raw.b54)
     var num = 0
     var numeros = ArrayList<Int>()
     var hilo =  HiloBarajear(this)
@@ -33,11 +43,16 @@ class MainActivity : AppCompatActivity() {
 
         //boton barajear
         barajear.setOnClickListener {
-            hilo.start()
-            Toast.makeText(this, "Se completo el barajeo", Toast.LENGTH_LONG)
-                .show()
-            comenzar.isEnabled = true
-            barajear.isEnabled = false
+            try{
+                //numeros.clear()
+                hilo.start()
+                Toast.makeText(this, "Se completo el barajeo", Toast.LENGTH_LONG)
+                    .show()
+                comenzar.isEnabled = true
+                barajear.isEnabled = false
+            }catch (e: Exception){
+                Toast.makeText(this, "error", Toast.LENGTH_LONG)
+            }
         }
 
         comenzar.setOnClickListener {
@@ -64,6 +79,7 @@ class MainActivity : AppCompatActivity() {
             mostrarImagen()
             terminada = false
             restantes.isEnabled = false
+            barajear.isEnabled = true
         }
 
     }
@@ -138,7 +154,7 @@ class HiloBarajear(puntero:MainActivity) : Thread(){
     var cont = 0
     override fun run() {
         super.run()
-        p.cartas.isEmpty()
+        //p.numeros.clear()
         while (cont < p.cartas.size) {
             p.num = (Math.random()*6+1).toInt()
             runCatching {
